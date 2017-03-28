081c8dd638f5f0dd456ca56a488581159ecf3f76
davik3000 <davik3000@users.noreply.github.com>
2017-03-28 14:34:13 +0200

    Updated scripts with SUDOCMD env variable

--- config/scripts/applyMotD.sh
+++ config/scripts/applyMotD.sh
@@ -7,9 +7,18 @@ echo "
 # dk-vagrant-centos #
 #####################
 Host: ${HOSTNAME}
-
 " > /etc/motd
 
-# Enable MotD
+# Enable MotD, if disabled
 sed -i -e 's/^PrintMotd no/PrintMotd yes/' /etc/ssh/sshd_config
-systemctl reload sshd
+
+# DART disabled, nice to have
+# it's too complex for its simple purpose: load a banner on ssh login
+#echo " > reloading sshd"
+#systemctl -q reload sshd
+#
+#systemctl -q show sshd
+#if [ $? -ne 0 ] ; then
+#  echo " > cannot reload, restarting sshd"
+#  systemctl -q restart sshd
+#fi
--- config/scripts/installJenkins.sh
+++ config/scripts/installJenkins.sh
@@ -4,27 +4,33 @@ JENKINS_REPO_URL="https://pkg.jenkins.io/redhat/jenkins.repo"
 JENKINS_REPO_PATH="/etc/yum.repos.d/jenkins.repo"
 JENKINS_KEY_URL="https://pkg.jenkins.io/redhat/jenkins.io.key"
 
+WHOAMI=$(whoami)
+SUDOCMD=""
+if [ "${WHOAMI}" != "root" ] ; then
+  SUDOCMD="sudo -E"
+fi
+
 #############
 # Functions #
 #############
-install_jenkins() {
+installJenkins() {
   echo "Installing Jenkins"
   echo "> yum: install epel-release"
-  sudo -E yum install -y -q epel-release
+  ${SUDOCMD} yum install -y -q epel-release
   
   echo "> wget: add Jenkins repository to /etc/yum.repos.d"
-  sudo -E wget -q -O "${JENKINS_REPO_PATH}" "${JENKINS_REPO_URL}"
+  ${SUDOCMD} wget -q -O ${JENKINS_REPO_PATH} ${JENKINS_REPO_URL}
   
   echo "> yum: import Jenkins GPG key"
-  sudo -E rpm --import "${JENKINS_KEY_URL}"
+  ${SUDOCMD} rpm --import ${JENKINS_KEY_URL}
 
   echo "> yum: install Jenkins"
-  sudo -E yum install -y -q jenkins
+  ${SUDOCMD} yum install -y -q jenkins
 }
 
 ########
 # Main #
 ########
-install_jenkins
+installJenkins
 
 echo "-----"
